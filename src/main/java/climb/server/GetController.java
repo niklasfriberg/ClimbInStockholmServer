9648e9ff00cd35b42cab3b8342b3a8f69805f841
niklasfriberg <niklasfribergprojekt@gmail.com>
2019-04-25 14:43:49 +0200

    2nd try

---
+++ src/main/java/climb/server/GetController.java
@@ -0,0 +1,18 @@
+package climb.server;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GetController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/hello")
+    public String helloWorld (){
+        return "Hello World!";
+    }
+}
