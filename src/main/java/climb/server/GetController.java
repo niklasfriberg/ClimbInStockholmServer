44d426d31dedceeca186fbefd42402989e953354
niklasfriberg <niklasfribergprojekt@gmail.com>
2019-04-25 15:04:45 +0200

    Hello world 2nd try

--- src/main/java/climb/server/GetController.java
+++ src/main/java/climb/server/GetController.java
@@ -2,17 +2,17 @@ package climb.server;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GetController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    @RequestMapping("/hello")
-    public String helloWorld (){
-        return "Hello World!";
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
