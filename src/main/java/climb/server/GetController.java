e49d03c4ea4ef1c6ffa29996b31f1c7f2ac5c2eb
niklasfriberg <niklasfribergprojekt@gmail.com>
2019-04-29 12:55:01 +0200

    tried enabling .war

--- src/main/java/climb/server/GetController.java
+++ src/main/java/climb/server/GetController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GetController {
 
-    @GetMapping("/greeting")
+    @RequestMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
