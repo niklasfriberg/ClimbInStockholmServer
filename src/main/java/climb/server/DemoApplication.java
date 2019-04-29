145d349f3516f8e6a13fa651de459d99c1c97ced
niklasfriberg <niklasfribergprojekt@gmail.com>
2019-04-29 15:20:04 +0200

    6th try

--- src/main/java/climb/server/DemoApplication.java
+++ src/main/java/climb/server/DemoApplication.java
@@ -2,6 +2,7 @@ package climb.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -9,6 +10,11 @@ public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
 	}
 
 }
