ec1fb7d90a25cac353686b9bd47f41749f407cec
niklasfriberg <niklasfribergprojekt@gmail.com>
2019-05-03 11:30:40 +0200

    closed SQL connection

--- src/main/java/climb/server/GetController.java
+++ src/main/java/climb/server/GetController.java
@@ -8,44 +8,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class GetController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
 
-    @GetMapping("/allUsers")
-    public String allUsers(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return getAllUsers();
-    }
-    
-    
+	@GetMapping("/allUsers")
+	public String allUsers(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return getAllUsers();
+	}
 
 	public String getAllUsers() {
 		StringBuilder user = null;
-		
-		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://mysql.dsv.su.se:3306/vady6245","vady6245","lie1NaWaeWai");
-			Statement stmt=con.createStatement();
+			Connection con = DriverManager.getConnection("jdbc:mysql://mysql.dsv.su.se:3306/vady6245", "vady6245",
+					"lie1NaWaeWai");
+			Statement stmt = con.createStatement();
 			String sql = "Select * from Users";
-			ResultSet rs= stmt.executeQuery(sql);
-			
-			
-			while(rs.next()) {
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
 				user.append(rs.getString("Name") + ", " + rs.getString("Password") + "\n");
+			}
+			con.close();
+		} catch (Exception e) {
+			System.out.print(e);
 		}
-		} catch (Exception e) {System.out.print(e);}
 		return user.toString();
-		}
+	}
 }
-
-
-
-
