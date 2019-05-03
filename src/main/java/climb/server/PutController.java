25f770993006dcf9105f305138a556410ec969de
niklasfriberg <niklasfribergprojekt@gmail.com>
2019-05-03 13:15:33 +0200

    small fix on putcontroller

--- src/main/java/climb/server/PutController.java
+++ src/main/java/climb/server/PutController.java
@@ -6,14 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class PutController {
 
-    @PutMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    @PutMapping("/updateUser")
+    public String putUser(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
