5589b33fd007d216a1bdf9dcf4c6197520a51a14
vady6245 <>
2019-05-03 15:42:56 +0200

    forsätter och testar

--- src/main/java/climb/server/UserRepository.java
+++ src/main/java/climb/server/UserRepository.java
@@ -2,7 +2,7 @@ package climb.server;
 
 import org.springframework.data.repository.CrudRepository;
 
-import hello.User;
+import climb.server.User;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
