81a81afa1a741140759dab558a91b56ddd7804ba
niklasfriberg <niklasfribergprojekt@gmail.com>
2019-05-03 14:13:01 +0200

    changes

--- src/main/java/climb/server/GetController.java
+++ src/main/java/climb/server/GetController.java
@@ -12,9 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GetController {
 
 	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-			Model model) {
-		model.addAttribute("name", name);
+	public String greeting() {
 		return "greeting";
 	}
 
