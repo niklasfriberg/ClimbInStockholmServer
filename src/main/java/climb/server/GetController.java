e9028568b7d59a6a4549c2a6935d9cfcb5bb9e77
niklasfriberg <niklasfribergprojekt@gmail.com>
2019-05-03 18:36:05 +0200

    tog bort import climb.server.User

--- src/main/java/climb/server/GetController.java
+++ src/main/java/climb/server/GetController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import climb.server.User; //testa
+// import climb.server.User; //testa
 //import climb.server.UserRepository; //testa
 
 @RestController
