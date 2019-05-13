0dbc5941895966bfdb956dae858cd4142d99e6c1
niklasfriberg <niklasfribergprojekt@gmail.com>
2019-05-13 14:26:06 +0200

    WIP3

--- src/main/java/climb/server/DemoApplication.java
+++ src/main/java/climb/server/DemoApplication.java
@@ -1,11 +1,16 @@
 package climb.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+	ErrorMvcAutoConfiguration.class
+   })
 public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
