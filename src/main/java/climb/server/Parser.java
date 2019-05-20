77225608e41c943404fa5232f91818636c74a321
niklasfriberg <niklasfribergprojekt@gmail.com>
2019-05-20 11:09:43 +0200

    Added vahabs code

--- src/main/java/climb/server/Parser.java
+++ src/main/java/climb/server/Parser.java
@@ -3,24 +3,76 @@ package climb.server;
 import org.springframework.http.converter.json.*;
 
 import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import org.my.schema.external.net.opengis.kml.*;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class Parser {
-    
-    public Parser(){
+
+    public static void parse() {
+
+        System.out.println("hej");
         try {
-            JAXBContext jc = JAXBContext.newInstance(PlacemarkType.class);
-            Unmarshaller u = jc.createUnmarshaller();
-            Marshaller m = jc.createMarshaller();
-            PlacemarkType pm = (PlacemarkType) u.unmarshal(new File("src/main/resources/Stockholm.kml"));
-            System.out.println(pm.toString());
-        } catch (Exception e) {
-            System.out.println(e.toString());
+            File stocks = new File("Stockholm.kml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(stocks);
+            doc.getDocumentElement().normalize();
+
+            System.out.println("root of xml file" + doc.getDocumentElement().getNodeName());
+            NodeList nodes = doc.getElementsByTagName("*");
+            System.out.println("==========================");
+
+            for (int i = 0; i < nodes.getLength(); i++) {
+                Node node = nodes.item(i);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element element = (Element) node;
+                    System.out.println(element.toString() + " : " + getValue("*", element));
+                    listAllAttributes(element);
+                }
+            }
+            System.out.println("hej");
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
-    
+
+    private static String getValue(String tag, Element element) {
+        try {
+
+            NodeList nodes = element.getElementsByTagName("*").item(0).getChildNodes();
+            Node node = (Node) nodes.item(0);
+            return node.getNodeValue().toString();
+        } catch (Exception e) {
+            return "inga koordinater";
+        }
+    }
+    public static void listAllAttributes(Element element) {
+
+        System.out.println("List attributes for node: " + element.getNodeName());
+
+        // get a map containing the attributes of this node
+            NamedNodeMap attributes = element.getAttributes();
+
+        // get the number of nodes in this map
+
+        int numAttrs = attributes.getLength();
+
+
+        for (int i = 0; i < numAttrs; i++) {
+       Attr attr = (Attr) attributes.item(i);
+       String attrName = attr.getNodeName();
+
+String attrValue = attr.getNodeValue();
+System.out.println("Found attribute: " + attrName + " with value: " + attrValue);
+        }
+    }
 }
