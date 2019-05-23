5e28c645e5822235f2e016a4307e11b1d98efc90
niklasfriberg <niklasfribergprojekt@gmail.com>
2019-05-23 11:32:19 +0200

    filepath spike

--- src/main/java/climb/server/GetController.java
+++ src/main/java/climb/server/GetController.java
@@ -66,7 +66,7 @@ public class GetController {
 		JSONObject crag = null;
 		JSONObject route;
 		StringBuilder sb = new StringBuilder();
-		File test = new File("."+File.pathSeparator+"main"+File.pathSeparator+"resources"+File.pathSeparator+"Stockholm.gpx");
+		File test = new File("var"+File.separator+"lib"+File.pathSeparator+"tomcat8"+File.pathSeparator+"src"+File.separator+"main"+File.pathSeparator+"resources"+File.pathSeparator+"Stockholm.gpx");
 		sb.append(test.exists()+"\n"+new File("").getAbsolutePath());
 		// for (int i = 0; i < xml.getLength();) {
 		// 	if (xml.isCrag(i)) {
