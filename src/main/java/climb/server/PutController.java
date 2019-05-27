816f7b2760b32edc8b719d1983a5ee2c37d49ea0
bubblanAB7 <bubblanab7@gmail.com>
2019-05-27 13:43:34 +0200

    plzplzplz

--- src/main/java/climb/server/PutController.java
+++ src/main/java/climb/server/PutController.java
@@ -60,19 +60,19 @@ public class PutController {
 			JSONArray apiResult = getCragsFromAPI();
 			for(int i = 0; i < apiResult.length(); i++){
 				updateDB(String.format("INSERT INTO Crag_API VALUES ('%d', '%d', '%s', '%s')", 
-				apiResult.getJSONObject(i).get("Longitud"), 
-				apiResult.getJSONObject(i).get("Latitud"),
-				apiResult.getJSONObject(i).get("CragName"),
+				apiResult.getJSONObject(i).getDouble("Longitud"), 
+				apiResult.getJSONObject(i).getDouble("Latitud"),
+				apiResult.getJSONObject(i).getString("CragName"),
 				"Innehåller 3 routes"
 				));
 				for (int j = 0; j < apiResult.getJSONObject(i).getJSONArray("Route").length(); j++){
 					updateDB(String.format("INSERT INTO Route_API VALUES ('%s', '%s', '%s', '%s', '%d', '%s')", 
-					apiResult.getJSONObject(i).getJSONArray("Route").getJSONObject(j).get("RouteName"),
-					apiResult.getJSONObject(i).get("CragName"),
-					apiResult.getJSONObject(i).getJSONArray("Route").getJSONObject(j).get("Höjd"),
-					apiResult.getJSONObject(i).getJSONArray("Route").getJSONObject(j).get("Svårighet"),
+					apiResult.getJSONObject(i).getJSONArray("Route").getJSONObject(j).getString("RouteName"),
+					apiResult.getJSONObject(i).getString("CragName"),
+					apiResult.getJSONObject(i).getJSONArray("Route").getJSONObject(j).getString("Höjd"),
+					apiResult.getJSONObject(i).getJSONArray("Route").getJSONObject(j).getString("Svårighet"),
 					0,
-					apiResult.getJSONObject(i).getJSONArray("Route").getJSONObject(j).get("Beskrivning")
+					apiResult.getJSONObject(i).getJSONArray("Route").getJSONObject(j).getString("Beskrivning")
 					));
 				}
 			}
