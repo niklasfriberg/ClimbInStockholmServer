5f0c847fca41dd941a90eb9e8b10227a48f21c54
niklasfriberg <niklasfribergprojekt@gmail.com>
2019-05-28 13:58:41 +0200

    test commit updateCragsfromAPI

--- src/main/java/climb/server/PutController.java
+++ src/main/java/climb/server/PutController.java
@@ -109,10 +109,12 @@ public class PutController {
 						if (xml.hasCoords(i)) {
 							route = new JSONObject();
 							hasRoutes = true;
-							route.accumulate("Svårighet", xml.getGrade(i));
+							route.accumulate("Svårighet", //xml.getGrade(i));
+							"7b");
 							route.accumulate("RouteName", xml.getName(i));
 							route.accumulate("Beskrivning", xml.getDesc(i));
-							route.accumulate("Höjd", xml.getHeigth(i));
+							route.accumulate("Höjd", //xml.getHeigth(i));
+							"10m");
 							crag.accumulate("Route" , route);
 							crag.put("Longitud", xml.getLng(i));
 							crag.put("Latitud", xml.getLat(i));
