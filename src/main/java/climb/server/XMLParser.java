6effb6521f4b4b128c2e58f3ecc9dbffae445f74
niklasfriberg <niklasfribergprojekt@gmail.com>
2019-05-28 15:19:14 +0200

    spike commit

--- src/main/java/climb/server/XMLParser.java
+++ src/main/java/climb/server/XMLParser.java
@@ -17,7 +17,8 @@ public class XMLParser {
 
     public XMLParser(String filename) {
         try {
-            URL url = new URL("https://raw.githubusercontent.com/niklasfriberg/ClimbInStockholmServer/master/src/main/resources/Stockholm.gpx");
+            URL url = new URL(
+                    "https://raw.githubusercontent.com/niklasfriberg/ClimbInStockholmServer/master/src/main/resources/Stockholm.gpx");
             URLConnection connection = url.openConnection();
             InputStream in = connection.getInputStream();
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -30,14 +31,15 @@ public class XMLParser {
         }
     }
 
-    public File downloadFile() throws Exception{
-        URL url = new URL("https://raw.githubusercontent.com/niklasfriberg/ClimbInStockholmServer/master/src/main/resources/Stockholm.gpx");
+    public File downloadFile() throws Exception {
+        URL url = new URL(
+                "https://raw.githubusercontent.com/niklasfriberg/ClimbInStockholmServer/master/src/main/resources/Stockholm.gpx");
         URLConnection connection = url.openConnection();
         InputStream in = connection.getInputStream();
-        File f = new File("data0"+File.separator+"Group75"+File.separator+"download.gpx");
+        File f = new File("data0" + File.separator + "Group75" + File.separator + "download.gpx");
         FileOutputStream fos = new FileOutputStream(f);
         byte[] buf = new byte[512];
-        while(true){
+        while (true) {
             int len = in.read(buf);
             if (len == -1) {
                 break;
@@ -49,9 +51,10 @@ public class XMLParser {
         fos.close();
         return f;
     }
-    //testkod
-    public String getDocument(){
-        if (document == null){
+
+    // testkod
+    public String getDocument() {
+        if (document == null) {
             return "hej jag är null";
         }
         return document.getXmlEncoding();
@@ -97,62 +100,68 @@ public class XMLParser {
     }
 
     public int getLength() {
-        if(document == null)
+        if (document == null)
             return 0;
         return document.getElementsByTagName("wpt").getLength();
     }
 
-    public String getGrade(int index){
+    public String getGrade(int index) {
         String trimmedString = getName(index).trim();
         trimmedString = trimmedString.replaceAll("\\*", "");
-        String[] tokens = trimmedString.split("\\s",0);
+        String[] tokens = trimmedString.split("\\s", 0);
         String grade = "";
         try {
-        for(int i=0; i < tokens.length; i++){
-            if(Character.isUpperCase(tokens[i].charAt(0))){
-                break;
+            for (int i = 0; i < tokens.length; i++) {
+                if (Character.isUpperCase(tokens[i].charAt(0))) {
+                    break;
+                }
+                grade += tokens[i] + " ";
             }
-            grade += tokens[i] + " ";
+        } catch (Exception e) {
+            return "No grade";
         }
-    } catch (Exception e){
-        return "No grade";
-    }
         return grade;
     }
 
-    public String getHeigth(int index){
-        String[] tokens = getName(index).split("\\s",0);
+    public String getHeigth(int index) {
+        String trimmedString = getName(index).trim();
+        trimmedString.replaceAll("\\*", "");
+        String[] tokens = getName(index).split("\\s", 0);
         String heigth = "";
         int j = 0;
-        for(int i=0; i < tokens.length; i++){
-            if(tokens[i].startsWith("(")){
-                heigth += tokens[i];
-                j = i+1;
-                break;
+        try {
+            for (int i = 0; i < tokens.length; i++) {
+                if (tokens[i].startsWith("(")) {
+                    heigth += tokens[i];
+                    j = i + 1;
+                    break;
+                }
             }
-        }
-        if(j != 0){
-            for(int i = j ; i < tokens.length; i++){
-                heigth += " " + tokens[i];
+            if (j != 0) {
+                for (int i = j; i < tokens.length; i++) {
+                    heigth += " " + tokens[i];
+                }
             }
+        } catch (Exception e) {
+            return "No height found";
         }
         return heigth;
     }
 
-    public String getName(String str){
-        String[] tokens = str.split("\\s",0);
+    public String getName(String str) {
+        String[] tokens = str.split("\\s", 0);
         String name = "";
         int j = 0;
-        for(int i=0; i < tokens.length; i++){
-            if(Character.isUpperCase(tokens[i].charAt(0))&& name == ""){
+        for (int i = 0; i < tokens.length; i++) {
+            if (Character.isUpperCase(tokens[i].charAt(0)) && name == "") {
                 name += tokens[i];
                 j = i + 1;
                 break;
             }
 
         }
-        for(int i = j ; i < tokens.length; i++){
-            if(tokens[i].startsWith("(")){
+        for (int i = j; i < tokens.length; i++) {
+            if (tokens[i].startsWith("(")) {
                 break;
             }
             name += " " + tokens[i];
@@ -160,7 +169,6 @@ public class XMLParser {
         }
         return name;
     }
-
 
     public boolean hasCoords(int i) {
         return getLat(i) != 0;
